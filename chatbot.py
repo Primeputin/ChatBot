98158375fb1ca281f9729d5655da9397a6704b43
Vladimir <vladimirtang@gmail.com>
2023-10-31 01:37:29 +0800

    asserting facts is checked by querying with the use of pyswip

--- chatbot.py
+++ chatbot.py
@@ -3,12 +3,163 @@ import re
 
 kb = Prolog()
 kb.consult("knowledge_base.pl")
+
+simplified = {'brother': 'sibling', 'sister': 'sibling', 'father': 'parent', 'mother': 'parent',
+              'grandfather': 'grandparent', 'grandmother': 'grandparent', 'son': 'child_of',
+              'daughter': 'child_of', 'child': 'child_of', 'siblings': 'sibling',
+              'uncle': 'uncle', 'aunt': 'aunt'}
+
 def make_lower(groups):
     return tuple(element.lower() for element in groups)
 def show_relations(q):
     for i in q:
         print(i["X"])
 
+def is_male(relation):
+    if relation == 'brother' or relation == 'father' or relation == 'grandfather' or relation == 'son' or relation == 'uncle':
+        return True
+    return False
+
+def is_female(relation):
+    if relation == 'sister' or relation == 'mother' or relation == 'grandmother' or relation == 'daughter' or relation == 'aunt':
+        return True
+    return False
+
+def tell_response(kb, statement, person1, relation, person2):
+    if person1 == person2:
+        return -1 # contradiction
+    if bool(list(kb.query(statement))):
+        return 1 # entailment
+    gender = morf(kb, person1, relation)
+    right = bool(list(kb.query(f'{simplified[relation]}({person1}, {person2})')))
+    not_right = bool(list(kb.query(f'not_{simplified[relation]}({person1}, {person2})')))
+    if right and not not_right and gender != -1:
+        return 1 # entailment
+    if not right and not_right or gender == -1:  
+        return -1 # contradiction
+    else:
+        kb.assertz(f'{simplified[relation]}({person1}, {person2})')
+        return 0 # contingency   
+    
+def tell(response):
+    if response == -1:
+        print("That's impossible!")  # contradiction
+    elif response == 0:
+        print("Ok, I learned something.") # contingency
+    elif response == 1:
+        print("I already knew that") # entailment
+
+def morf(kb, person, relation):
+    if is_male(relation):
+        male = bool(list(kb.query(f'male({person})')))
+        not_male = bool(list(kb.query(f'not_male({person})')))
+        if male and not not_male:
+            return 2 # meaning male fact already exist
+        elif not male and not not_male:
+            kb.assertz(f'male({person})')
+            kb.assertz(f'not(not_male({person}))')
+            return 1 # meaning male fact is asserted
+        elif not male and not_male:
+            return -1 # not male
+    elif is_female(relation):
+        male = bool(list(kb.query(f'male({person})')))
+        not_male = bool(list(kb.query(f'not_male({person})')))
+        if not_male and not male:
+            return 2 # meaning female fact already exist
+        elif not not_male and not male:
+            kb.assertz(f'not_male({person})')
+            kb.assertz(f'not(male({person}))')
+            return 1 # meaning female fact is asserted
+        elif not not_male and male:
+            return -1 # not female
+    return 0 # meaning no gender needed for the relation
+
+def compound_respond(kb, people, relation, person):
+    learned = False
+    for i in people:
+        check = f'{relation}({person}, {i})'
+        response = check_tell_response(kb, check, i, relation, person)
+        if response == 0:
+            learned = True
+        elif response == -1:
+            break
+    if learned and response != -1:
+        for i in people:
+            check = f'{relation}({person}, {i})'
+            tell_response(kb, check, i, relation, person)
+        return 0 # contingency
+    elif not learned and response != -1:
+        return 1 # entailment
+    else:
+        return -1 # contadiction
+
+# doesn't assert anything, just checking
+def check_tell_response(kb, statement, person1, relation, person2):
+    if person1 == person2:
+        return -1 # contradiction
+    if bool(list(kb.query(statement))):
+        return 1 # entailment
+    gender = check_morf(kb, person1, relation)
+    right = bool(list(kb.query(f'{simplified[relation]}({person1}, {person2})')))
+    not_right = bool(list(kb.query(f'not_{simplified[relation]}({person1}, {person2})')))
+    if right and not not_right and gender != -1:
+        return 1 # entailment
+    if not right and not_right or gender == -1:  
+        return -1 # contradiction
+    else:
+        return 0 # contingency
+
+# doesn't assert anything, just checking 
+def check_morf(kb, person, relation):
+    if is_male(relation):
+        male = bool(list(kb.query(f'male({person})')))
+        not_male = bool(list(kb.query(f'not_male({person})')))
+        if male and not not_male:
+            return 2 # meaning male fact already exist
+        elif not male and not not_male:
+            return 1 # meaning male fact is asserted
+        elif not male and not_male:
+            return -1 # not male
+    elif is_female(relation):
+        male = bool(list(kb.query(f'male({person})')))
+        not_male = bool(list(kb.query(f'not_male({person})')))
+        if not_male and not male:
+            return 2 # meaning female fact already exist
+        elif not not_male and not male:
+            return 1 # meaning female fact is asserted
+        elif not not_male and male:
+            return -1 # not female
+    return 0 # meaning no gender needed for the relation
+
+def ask_response(kb, person1, relation, person2):
+    if person1 == person2:
+        print('No')
+    else:
+        right = bool(list(kb.query(f"{relation}({person1}, {person2})")))
+        not_right = bool(list(kb.query(f"not_{relation}({person1}, {person2})")))
+        if right and not not_right:
+            print('Yes')
+        elif not right and not not_right:
+            print('I don\'t know')
+        elif not right and not_right:
+            print('No')
+def compound_ask(kb, people, relation, person):
+    rel = ''
+    not_rel = ''
+    for i in people:
+        rel += f'{relation}({i}, {person}),'
+        not_rel += f'not_{relation}({i}, {person});'
+    if rel and not_rel:
+        rel = rel[:-1]
+        not_rel = not_rel[:-1]
+        right = bool(list(kb.query(rel)))
+        not_right = bool(list(kb.query(not_rel)))
+        if right and not not_right:
+            print('Yes')
+        elif not right and not not_right:
+            print('I don\'t know')
+        elif not right and not not_right:
+            print('No')
 while (True):
     prompt = input("Prompt: ")
     isMatch = re.match(r"(\w+) is (?:the|a|an) (\w+) of (\w+)", prompt)
@@ -38,44 +189,46 @@ while (True):
         print("person 1: ", person1)
         print("relation: ", relation)
         print("person 2:", person2)
-        kb.assertz(f"{relation}({person1}, {person2})")
+        check = f"{relation}({person1}, {person2})"
+        tell(tell_response(kb, check, person1, relation, person2))
     elif sibsMatch:
         sib1, sib2 = make_lower(sibsMatch.groups())
         print("sibling 1: ", sib1)
         print("sibling 2: ", sib2)
-        kb.assertz(f"sibling({sib1}, {sib2})")
+        check = f"siblings({sib1}, {sib2})"
+        tell(tell_response(kb, check, sib1, 'siblings', sib2))
     elif parentsMatch: # statements regarding parents
         parent1, parent2, child = make_lower(parentsMatch.groups())
         print("parent 1: ", parent1)
         print("parent 2: ", parent2)
         print("child:", child)
-        kb.assertz(f"parent({parent1}, {child}), parent({parent2}, {child})")
+        tell(compound_respond(kb, [parent1, parent2], 'parent', child))
     elif childrenMatch: # statements regarding children
         child1, child2, child3, ancestor = make_lower(childrenMatch.groups())
         print("child 1: ", child1)
         print("child 2: ", child2)
         print("child 3: ", child3)
         print("ancestor: ", ancestor)
-        kb.assertz(f"child({child1}, {ancestor}), child({child2}, {ancestor}), child({child3}, {ancestor})")
+        tell(compound_respond(kb, [child1, child2, child3], 'child', ancestor))
     elif isQuestion:
         person1, relation, person2 = make_lower(isQuestion.groups())
-        print(bool(list(kb.query(f"{relation}({person1}, {person2})"))))
+        ask_response(kb, person1, relation, person2)
     elif areSibRel:
         person1, person2, relation = make_lower(areSibRel.groups())
         print(person1)
         print(relation)
         print(person2)
-        print(bool(list(kb.query(f"{relation}({person1}, {person2})"))))
+        ask_response(kb, person1, relation, person2)
     elif areParentsMatch:
         parent1, parent2, child = make_lower(areParentsMatch.groups())
-        print(bool(list(kb.query(f"parent({parent1}, {child}), parent({parent2}, {child})"))))
+        compound_ask(kb, [parent1, parent2], 'parent', child)
     elif areChildrenMatch: # statements regarding children
         child1, child2, child3, ancestor = make_lower(areChildrenMatch.groups())
         print("child 1: ", child1)
         print("child 2: ", child2)
         print("child 3: ", child3)
         print("ancestor: ", ancestor)
-        print(bool(list(kb.query(f"child({child1}, {ancestor}), child({child2}, {ancestor}), child({child3}, {ancestor})"))))
+        compound_ask(kb, [child1, child2, child3], 'child', ancestor)
     elif whoSibs:
         person = whoSibs.group(1).lower() # getting the inputted match only
         show_relations(list(kb.query(f"siblings(X, {person})")))
--- knowledge_base.pl
+++ knowledge_base.pl
@@ -1,27 +1,162 @@
-:- dynamic grandfather/2.
-:- dynamic grandmother/2.
-:- dynamic uncle/2.
-:- dynamic aunt/2.
-:- dynamic son/2.
-:- dynamic daughter/2.
-:- dynamic father/2.
-:- dynamic mother/2.
-:- dynamic brother/2.
-:- dynamic sister/2.
-:- dynamic siblings/2.
-:- dynamic relatives/2.
+:- dynamic grandparent/2.
+:- dynamic not_grandparent/2.
 
-parent(X, Y) :- father(X, Y).
-parent(X, Y) :- mother(X, Y).
+:- dynamic grandfather/2.
+:- dynamic not_grandfather/2.
+
+:- dynamic grandmother/2.
+:- dynamic not_grandmother/2.
+
+:- dynamic uncle/2.
+:- dynamic not_uncle/2.
+
+:- dynamic aunt/2.
+:- dynamic not_aunt/2.
+
+:- dynamic son/2.
+:- dynamic not_son/2.
+
+:- dynamic daughter/2.
+:- dynamic not_daughter/2.
+
+:- dynamic father/2.
+:- dynamic not_father/2.
+
+:- dynamic mother/2.
+:- dynamic not_mother/2.
+
+:- dynamic brother/2.
+:- dynamic not_brother/2.
+
+:- dynamic sister/2.
+:- dynamic not_sister/2.
+
+:- dynamic parent/2.
+:- dynamic not_parent/2.
+
+:- dynamic sibling/2.
+:- dynamic not_sibling/2.
+
+:- dynamic siblings/2.
+:- dynamic not_siblings/2.
+
+:- dynamic relatives/2.
+:- dynamic not_relatives/2.
+
+:- dynamic child_of/2.
+:- dynamic not_child_of/2.
+
+:- dynamic child/2.
+:- dynamic not_child/2.
+
+:- dynamic male/1.
+:- dynamic not_male/1.
+
+
+child(X, Y) :- child_of(X, Y).
 child(X, Y) :- parent(Y, X).
+not_child_of(X, Y) :- X = Y.
+not_child_of(X, Y) :- child_of(Y, X).
+not_child_of(X, Y) :- parent(X, Y).
+not_child_of(X, Y) :- siblings(X, Y).
+not_child_of(X, Y) :- grandparent(X, Y), grandparent(Y, X).
+not_child_of(X, Y) :- uncle(X, Y); uncle(Y, X).
+not_child_of(X, Y) :- aunt(X, Y); aunt(Y, X).
+not_child(X, Y) :- not_child_of(X, Y).
+
+parent(X, Y) :- child_of(Y,X).
+not_parent(X, Y) :- child(X, Y).
+not_parent(X, Y) :- X = Y.
+not_father(X, Y) :- not_parent(X, Y).
+not_father(X, _) :- not_male(X).
+not_mother(X, Y) :- not_parent(X, Y).
+not_mother(X, _) :- male(X).
+
+son(X, Y) :- child(X, Y), male(X).
+not_son(X, _) :- not_male(X).
+not_son(X, Y) :- not_child(X, Y).
+daughter(X, Y) :- child(X, Y), not_male(X).
+not_daughter(X, _) :- male(X).
+not_daughter(X, Y) :- not_child(X, Y).
+
+siblings(Person1, Person2) :- sibling(Person1, Person2).
+siblings(Person1, Person2) :- sibling(Person2, Person1).
 siblings(Person1, Person2) :- parent(X, Person1), parent(X, Person2), Person1 \= Person2.
-siblings(Person1, Person2) :- brother(Person1, Person2). % the four lines are reflexive relationship for siblings
-siblings(Person1, Person2) :- brother(Person2, Person1).
-siblings(Person1, Person2) :- sister(Person1, Person2).
-siblings(Person1, Person2) :- sister(Person2, Person1).
+not_sibling(Person1, Person2) :- Person1 = Person2.
+not_sibling(Person1, Person2) :- parent(Person1, Person2).
+not_sibling(Person1, Person2) :- child(Person1, Person2).
+not_sibling(Person1, Person2) :- grandparent(Person1, Person2); grandparent(Person2, Person1).
+not_sibling(Person1, Person2) :- uncle(Person1, Person2); uncle(Person2, Person1).
+not_sibling(Person1, Person2) :- aunt(Person1, Person2); uncle(Person2, Person1).
+
+
+not_siblings(Person1, Person2) :- not_sibling(Person1, Person2).
+not_siblings(Person1, Person2) :- not_sibling(Person2, Person1).
+not_brother(Person1, Person2) :- not_siblings(Person1, Person2).
+not_brother(Person1, _) :- not_male(Person1).
+not_sister(Person1, Person2) :- not_siblings(Person1, Person2).
+not_sister(Person1, _) :- male(Person1).
+
+father(Person1, Person2) :-
+    male(Person1),          % Check if Person1 is a male
+    Person1 \= Person2,     % Check if Person1 and Person2 are not the same
+    parent(Person1, Person2).  
+mother(Person1, Person2) :-
+    not_male(Person1),          % Check if Person1 is a female
+    Person1 \= Person2,     % Check if Person1 and Person2 are not the same
+    parent(Person1, Person2).  
+brother(Person1, Person2) :-
+    male(Person1),          % Check if Person1 is a male
+    Person1 \= Person2,     % Check if Person1 and Person2 are not the same
+    siblings(Person1, Person2). % Check if they are siblings 
+sister(Person1, Person2) :-
+    not_male(Person1),          % Check if Person1 is a female
+    Person1 \= Person2,     % Check if Person1 and Person2 are not the same
+    siblings(Person1, Person2). % Check if they are siblings  
+
+grandparent(Person1, Person2) :- parent(Person1, Person), parent(Person, Person2), Person1 \= Person2.
+not_grandparent(Person1, Person2) :- grandparent(Person2, Person1).
+not_grandparent(Person1, Person2) :- siblings(Person1, Person2).
+not_grandparent(Person1, Person2) :- uncle(Person1, Person2); uncle(Person2, Person1).
+not_grandparent(Person1, Person2) :- aunt(Person1, Person2); aunt(Person2, Person1).
+not_grandparent(Person1, Person2) :- parent(Person1, Person2).
+not_grandparent(Person1, Person2) :- child(Person1, Person2).
+not_grandparent(Person1, Person2) :- Person1 = Person2.
+
+grandfather(Person1, Person2) :- 
+    male(Person1),
+    Person1 \= Person2,
+    grandparent(Person1, Person2).
+not_grandfather(Person1, _) :- not_male(Person1).
+grandmother(Person1, Person2) :- 
+    not_male(Person1),
+    Person1 \= Person2,
+    grandparent(Person1, Person2).
+not_grandmother(Person1, _) :- male(Person1).
+ 
+ % for some reason, can't use brother/2
+uncle(Person1, Person2) :- male(Person1), siblings(Person1, Person), parent(Person, Person2), Person1 \= Person2.
+not_uncle(Person1, _) :- not_male(Person1).
+not_uncle(Person1, Person2) :- grandparent(Person1, Person2); grandparent(Person2, Person1).
+not_uncle(Person1, Person2) :- siblings(Person1, Person2).
+not_uncle(Person1, Person2) :- uncle(Person2, Person1).
+not_uncle(Person1, Person2) :- aunt(Person1, Person2), aunt(Person2, Person1).
+not_uncle(Person1, Person2) :- parent(Person1, Person2).
+not_uncle(Person1, Person2) :- child(Person1, Person2).
+% for some reason, can't use sister/2
+aunt(Person1, Person2) :- not_male(Person1), siblings(Person1, Person), parent(Person, Person2), Person1 \= Person2.
+not_aunt(Person1, _) :- male(Person1).
+not_aunt(Person1, Person2) :- grandparent(Person1, Person2); grandparent(Person2, Person1).
+not_aunt(Person1, Person2) :- siblings(Person1, Person2).
+not_aunt(Person1, Person2) :- uncle(Person2, Person1), uncle(Person2, Person1).
+not_aunt(Person1, Person2) :- aunt(Person1, Person2).
+not_aunt(Person1, Person2) :- parent(Person1, Person2).
+not_aunt(Person1, Person2) :- child(Person1, Person2).
 
 relatives(Person1, Person2) :- siblings(Person1, Person2).
+relatives(Person1, Person2) :- grandparent(Person1, Person2); grandparent(Person2, Person1).
+relatives(Person1, Person2) :- uncle(Person1, Person2); uncle(Person2, Person1).
+relatives(Person1, Person2) :- aunt(Person1, Person2); aunt(Person2, Person1).
 relatives(Person1, Person2) :- parent(Person1, Person2).
-relatives(Person1, Person2) :- parent(Person2, Person1).
 relatives(Person1, Person2) :- child(Person1, Person2).
-relatives(Person1, Person2) :- child(Person2, Person1).
+not_relatives(Person1, Person2) :- Person1 = Person2.
