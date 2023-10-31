7f38f75f67ca1b8ffbeab11a99cdc9c8993fae72
Vladimir <vladimirtang@gmail.com>
2023-10-31 15:48:15 +0800

    matching of sentence patterns is more strict to prevent errors occuring

--- chatbot.py
+++ chatbot.py
@@ -162,15 +162,15 @@ def compound_ask(kb, people, relation, person):
             print('No')
 while (True):
     prompt = input("Prompt: ")
-    isMatch = re.match(r"(\w+) is (?:the|a|an) (\w+) of (\w+)", prompt)
-    sibsMatch = re.match(r"(\w+) and (\w+) are siblings", prompt)
-    parentsMatch = re.match(r"(\w+) and (\w+) are the parents of (\w+)", prompt)
-    childrenMatch = re.match(r"(\w+), (\w+), and (\w+) are children of (\w+)", prompt)
+    isMatch = re.match(r"^(\w+) is (?:the|a|an) ((?:father|mother|brother|sister|child|son|daughter|grandfather|grandmother|uncle|aunt)) of (\w+).$", prompt)
+    sibsMatch = re.match(r"^(\w+) and (\w+) are siblings.$", prompt)
+    parentsMatch = re.match(r"^(\w+) and (\w+) are the parents of (\w+).$", prompt)
+    childrenMatch = re.match(r"^(\w+), (\w+), and (\w+) are children of (\w+).$", prompt)
 
-    isQuestion = re.match(r"Is (\w+) (?:the|a|an) (\w+) of (\w+)?", prompt)
-    areSibRel = re.match(r"Are (\w+) and (\w+) (\w+)?", prompt)
-    areParentsMatch = re.match(r"Are (\w+) and (\w+) the parents of (\w+)?", prompt)
-    areChildrenMatch = re.match(r"Are (\w+), (\w+), and (\w+) children of (\w+)?", prompt)
+    isQuestion = re.match(r"^Is (\w+) (?:the|a|an) ((?:father|mother|brother|sister|child|son|daughter|grandfather|grandmother|uncle|aunt)) of (\w+)?$", prompt)
+    areSibRel = re.match(r"^Are (\w+) and (\w+) ((?:siblings|relatives))?$", prompt)
+    areParentsMatch = re.match(r"^Are (\w+) and (\w+) the parents of (\w+)?$", prompt)
+    areChildrenMatch = re.match(r"^Are (\w+), (\w+), and (\w+) children of (\w+)?$", prompt)
 
     whoSibs = re.match(r"Who are the siblings of (\w+)?", prompt)
     whoSis = re.match(r"Who are the sisters of (\w+)?", prompt)
@@ -186,29 +186,17 @@ while (True):
         show_relations(list(kb.query(f"{relation}(X, {person})")))
     elif isMatch: # simple is statement
         person1, relation, person2 = make_lower(isMatch.groups())
-        print("person 1: ", person1)
-        print("relation: ", relation)
-        print("person 2:", person2)
         check = f"{relation}({person1}, {person2})"
         tell(tell_response(kb, check, person1, relation, person2))
     elif sibsMatch:
         sib1, sib2 = make_lower(sibsMatch.groups())
-        print("sibling 1: ", sib1)
-        print("sibling 2: ", sib2)
         check = f"siblings({sib1}, {sib2})"
         tell(tell_response(kb, check, sib1, 'siblings', sib2))
     elif parentsMatch: # statements regarding parents
         parent1, parent2, child = make_lower(parentsMatch.groups())
-        print("parent 1: ", parent1)
-        print("parent 2: ", parent2)
-        print("child:", child)
         tell(compound_respond(kb, [parent1, parent2], 'parent', child))
     elif childrenMatch: # statements regarding children
         child1, child2, child3, ancestor = make_lower(childrenMatch.groups())
-        print("child 1: ", child1)
-        print("child 2: ", child2)
-        print("child 3: ", child3)
-        print("ancestor: ", ancestor)
         tell(compound_respond(kb, [child1, child2, child3], 'child', ancestor))
     elif isQuestion:
         person1, relation, person2 = make_lower(isQuestion.groups())
@@ -224,10 +212,6 @@ while (True):
         compound_ask(kb, [parent1, parent2], 'parent', child)
     elif areChildrenMatch: # statements regarding children
         child1, child2, child3, ancestor = make_lower(areChildrenMatch.groups())
-        print("child 1: ", child1)
-        print("child 2: ", child2)
-        print("child 3: ", child3)
-        print("ancestor: ", ancestor)
         compound_ask(kb, [child1, child2, child3], 'child', ancestor)
     elif whoSibs:
         person = whoSibs.group(1).lower() # getting the inputted match only
